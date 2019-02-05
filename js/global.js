bac3922cb506821ba4b2726e12e33608bb0fd48d
james kisuule <kisuulejames1@gmail.com>
2019-02-05 15:11:16 +0300

    add javascript to login[#163671093]

--- js/global.js
+++ js/global.js
@@ -1,2 +1,2 @@
-// var SERVER_PATH="https://ireporterapp33.herokuapp.com";
-var SERVER_PATH="http://127.0.0.1:5000/";
+var SERVER_PATH="https://ireporterapp33.herokuapp.com";
+// var SERVER_PATH="http://127.0.0.1:5000/";
