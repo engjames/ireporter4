edd7ab4e288204dded96495ad6fa7155549acc9e
james kisuule <kisuulejames1@gmail.com>
2019-02-07 16:33:46 +0300

    Update status of a record[#163671162]

--- js/myscript.js
+++ js/myscript.js
@@ -46,14 +46,43 @@ function clickOutside(e){
 	
 }
 
-function mydeleteFunction() {
-      confirm("Are you sure you want to delete this product!");
-  }
-
-function myRightsFunction() {
-      confirm("Are you sure you want to give admin rights");
-  }  
 }
+function mydeleteFunction(incident_id, category) {
+   //red-flag-category = 1
+   //intervention-category = 0
+   alert(incident_id+"-->"+category);
+   var response = confirm("Are you sure you want to delete this incident?");
+   if (response === true){
+      
+      if(category === 1){
+         var delete_url=SERVER_PATH+"redflags/"+incident_id;
+      }
+      else{
+         var delete_url=SERVER_PATH+"interventions/"+incident_id;
+      }
+     
 
-
-
+      fetch(delete_url, {
+         method: 'DELETE',
+         headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+            "auth_token":  localStorage.getItem("auth-token")
+         },
+         cache: 'no-cache'
+         
+      })
+         .then((res) => res.json())
+         .then(result => {
+            if(result.status === 200){
+               alert("Incident with id "+incident_id+" has been deleted")
+               
+            }
+            else{
+               alert(JSON.stringify(result.error))
+            }
+           
+         })
+  }
+}
+       
